b21fa74e0b5f5f136d6d77a18e16e7ea99e3d153
Marco van den Oever <marco@flyingpie.nl>
2018-07-24 19:12:38 +0200

    Open hosts file

---
+++ MLaunch.Plugins/Hosts/HostsCommandQueryExecutor.cs
@@ -0,0 +1,34 @@
+﻿using ImGuiNET;
+using MLaunch.Core.QueryExecutors.CommandQuery;
+using System.Diagnostics;
+
+namespace MLaunch.Plugins.Hosts
+{
+    public class HostsCommandQueryExecutor : CommandQueryExecutor
+    {
+        public override string[] CommandPrefixes => new[] { "hosts" };
+
+        public override string Description => "Opens the local hosts file";
+
+        public override string Name => "Hosts";
+
+        public override string ExampleUsage => CommandPrefixes[0];
+
+        public override bool TryExecute(string term)
+        {
+            Process.Start(new ProcessStartInfo()
+            {
+                FileName = "notepad",
+                Arguments = @"C:\Windows\System32\drivers\etc\hosts",
+                Verb = "RunAs"
+            });
+
+            return true;
+        }
+
+        public override void Draw()
+        {
+            ImGui.Text("Open hosts file");
+        }
+    }
+}
