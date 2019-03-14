226efb215bb7ca0e071b335ca1a600f1aef57f2c
Marco van den Oever <marco@flyingpie.nl>
2019-03-14 21:32:26 +0100

    Plugin - Time

---
+++ src/AVA.Plugins.Time/TimeZoneConversion/TimeConverterRules/BaseTimeConverterRule.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ExpNodaTime.TimeConverterRules
+{
+    public abstract class BaseTimeConverterRule : ITimeConverterRule
+    {
+        public abstract string Name { get; }
+
+        public abstract Regex Regex { get; }
+
+        public bool TryParse(TimeConverterContext context, out TimeConverterResult result)
+        {
+            result = null;
+
+            var match = Regex.Match(context.Expression);
+
+            if (!match.Success) return false;
+
+            return TryParseUtc(context, match, out result);
+        }
+
+        protected abstract bool TryParseUtc(TimeConverterContext context, Match match, out TimeConverterResult result);
+    }
+}
---
+++ src/AVA.Plugins.Time/TimeZoneConversion/TimeZone.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using TimeZoneNames;
+
+namespace ExpNodaTime
+{
+    public class TimeZone
+    {
+        public string Id => TimeZoneInfo.Id;
+
+        public TimeZoneInfo TimeZoneInfo { get; set; }
+
+        public TimeZoneValues TimeZoneValues { get; set; }
+
+        public bool IsMatch(string expression) =>
+            (TimeZoneValues.Daylight?.Equals(expression, StringComparison.OrdinalIgnoreCase) ?? false)
+            ||
+            (TimeZoneValues.Generic?.Equals(expression, StringComparison.OrdinalIgnoreCase) ?? false)
+            ||
+            (TimeZoneValues.Standard?.Equals(expression, StringComparison.OrdinalIgnoreCase) ?? false)
+        ;
+
+        public static TimeZone FromTimeZoneInfo(TimeZoneInfo tzi, string languageCode)
+        {
+            return new TimeZone()
+            {
+                TimeZoneInfo = tzi,
+                TimeZoneValues = TZNames.GetAbbreviationsForTimeZone(tzi.Id, languageCode)
+            };
+        }
+    }
+}
