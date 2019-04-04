ae03b8e0092804f3f74a488a6855a9e9b9360dbc
Marco van den Oever <marco@flyingpie.nl>
2019-04-04 10:58:22 +0200

    Indexer - Ava.json

--- src/AVA.Indexing/Sources/FileSystem/FileSystemIndexerSource.cs
+++ src/AVA.Indexing/Sources/FileSystem/FileSystemIndexerSource.cs
@@ -1,4 +1,5 @@
 ﻿using MUI.DI;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -13,7 +14,7 @@ namespace AVA.Indexing.Sources.FileSystem
 
         public IEnumerable<IndexedItem> GetItems() => Settings.Paths
             .Select(f => Environment.ExpandEnvironmentVariables(f))
-            .GetFilesRecursive()
+            .GetFilesRecursiveX()
             .Select(path => new FileSystemIndexedItem()
             {
                 Name = Path.GetFileNameWithoutExtension(path),
@@ -24,5 +25,60 @@ namespace AVA.Indexing.Sources.FileSystem
             })
             .ToList()
         ;
+    }
+
+    public static class Ext
+    {
+        public static List<string> GetFilesRecursiveX(this IEnumerable<string> folders, List<string> files = null)
+        {
+            files = files ?? new List<string>();
+
+            foreach (var folder in folders)
+            {
+                try
+                {
+                    var subFiles = Directory.GetFiles(folder, "*", SearchOption.TopDirectoryOnly);
+
+                    if (TryGetAvaJson(subFiles, out var avaSettings))
+                    {
+                        if (!avaSettings.Index) continue;
+                    }
+
+                    foreach (var file in subFiles)
+                    {
+                        files.Add(file);
+                    }
+
+                    var dd = Directory.GetDirectories(folder);
+
+                    GetFilesRecursiveX(dd, files);
+                }
+                catch { }
+            }
+
+            return files;
+        }
+
+        public static bool TryGetAvaJson(string[] files, out AvaSettings settings)
+        {
+            settings = null;
+
+            var avaJson = files.FirstOrDefault(s => Path.GetFileName(s).Equals("ava.json", StringComparison.OrdinalIgnoreCase));
+
+            if (avaJson != null)
+            {
+                try
+                {
+                    settings = JsonConvert.DeserializeObject<AvaSettings>(File.ReadAllText(avaJson));
+                    return settings != null;
+                }
+                catch
+                {
+                    // TODO
+                }
+            }
+
+            return false;
+        }
     }
 }
--- src/AVA.Plugins/ClipboardHistory/ClipboardHistoryQueryExecutor.cs
+++ src/AVA.Plugins/ClipboardHistory/ClipboardHistoryQueryExecutor.cs
@@ -1,44 +1,44 @@
-﻿using AVA.Core.QueryExecutors;
-using AVA.Core.QueryExecutors.ListQuery;
-using FontAwesomeCS;
-using MUI;
-using MUI.DI;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿//using AVA.Core.QueryExecutors;
+//using AVA.Core.QueryExecutors.ListQuery;
+//using FontAwesomeCS;
+//using MUI;
+//using MUI.DI;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
 
-namespace AVA.Plugins.ClipboardHistory
-{
-    [Help(Name = "Clipboard history", Description = "View and reactivate past clips", ExampleUsage = "cc <term?>", Icon = FAIcon.CopyRegular)]
-    public class ClipboardHistoryQueryExecutor : ListQueryExecutor
-    {
-        [Dependency] public ClipboardService ClipboardService { get; set; }
+//namespace AVA.Plugins.ClipboardHistory
+//{
+//    [Help(Name = "Clipboard history", Description = "View and reactivate past clips", ExampleUsage = "cc <term?>", Icon = FAIcon.CopyRegular)]
+//    public class ClipboardHistoryQueryExecutor : ListQueryExecutor
+//    {
+//        [Dependency] public ClipboardService ClipboardService { get; set; }
         
-        public override string Prefix => "cc ";
+//        public override string Prefix => "cc ";
 
-        public override IEnumerable<IListQueryResult> GetQueryResults(string term) =>
-            ClipboardService
-            .History
-            .Select(h =>
-            {
-                var res = new ListQueryResult()
-                {
-                    Name = h.Timestamp.ToString("s"),
-                    Description = "",
-                    OnExecute = t => ClipboardService.Restore(h)
-                };
+//        public override IEnumerable<IListQueryResult> GetQueryResults(string term) =>
+//            ClipboardService
+//            .History
+//            .Select(h =>
+//            {
+//                var res = new ListQueryResult()
+//                {
+//                    Name = h.Timestamp.ToString("s"),
+//                    Description = "",
+//                    OnExecute = t => ClipboardService.Restore(h)
+//                };
 
-                if (!string.IsNullOrEmpty(h.Text))
-                {
-                    res.Description = string.Join("", h.Text.Replace(Environment.NewLine, "").Take(40));
-                }
+//                if (!string.IsNullOrEmpty(h.Text))
+//                {
+//                    res.Description = string.Join("", h.Text.Replace(Environment.NewLine, "").Take(40));
+//                }
 
-                if (h.ImageThumbnail != null)
-                {
-                    res.Icon = ResourceManager.Instance.LoadImage($"cb_{h.Timestamp.ToString("s")}", h.ImageThumbnail);
-                }
+//                if (h.ImageThumbnail != null)
+//                {
+//                    res.Icon = ResourceManager.Instance.LoadImage($"cb_{h.Timestamp.ToString("s")}", h.ImageThumbnail);
+//                }
 
-                return (IListQueryResult)res;
-            });
-    }
-}
+//                return (IListQueryResult)res;
+//            });
+//    }
+//}
