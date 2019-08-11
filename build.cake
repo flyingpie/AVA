f1a0141706d4e4ae1e10f0776a77e42248abfe18
Marco van den Oever <marco@flyingpie.nl>
2019-08-11 12:54:53 +0200

    Workspace

--- build.cake
+++ build.cake
@@ -21,9 +21,9 @@ Task("Default").Does(() =>
         Configuration = configuration,
         MaxCpuCount = 0, // As many as available
         NodeReuse = false, // Required to prevent build task dll's from being locked
-		PlatformTarget = platform,
+        //PlatformTarget = platform,
         ToolPath = GetFiles(VSWhereLatest() + "/**/MSBuild.exe").FirstOrDefault()
-    });
+    }.WithProperty("runtime", "win10-x64"));
 
     // TEST //
     VSTest("./bin/**/*.UnitTest.dll", new VSTestSettings
--- src/MUI/Win32/Extensions/ResourceManagerExtensions.cs
+++ src/MUI/Win32/Extensions/ResourceManagerExtensions.cs
@@ -1,67 +1,90 @@
 ﻿using MUI.Graphics;
 using MUI.Logging;
+using System;
 using System.IO;
-using Windows.Storage.FileProperties;
 
 namespace MUI.Win32.Extensions
 {
     public static class ResourceManagerExtensions
     {
-        public static Graphics.Image LoadImageFromIcon(this ResourceManager resourceManager, string path)
+        public static Image LoadImageFromIcon(this ResourceManager resourceManager, string path)
         {
             return resourceManager.LoadImage(path, textureLoader => new LazyImage(resourceManager.DefaultImage, () =>
             {
                 var _log = Log.Get<ResourceManager>();
 
-                // Normalize path
-                path = Path.GetFullPath(path);
-
-                _log.Info($"Loading image from icon at path '{path}'...");
-
-                using (var bmpStream = new MemoryStream())
+                try
                 {
-                    var bmp = ThumbnailLoader.GetThumbnail(path, 50, 50, ThumbnailOptions.None);
-                    bmp.Save(bmpStream, System.Drawing.Imaging.ImageFormat.Bmp);
+                    // Normalize path
+                    path = Path.GetFullPath(path);
 
-                    // Convert to texture
-                    return textureLoader.Load(bmpStream.ToArray());
+                    _log.Info($"Loading image from icon at path '{path}'...");
+
+                    using (var bmpStream = new MemoryStream())
+                    {
+                        var bmp = ThumbnailLoader.GetThumbnail(path, 50, 50, ThumbnailOptions.None);
+                        bmp.Save(bmpStream, System.Drawing.Imaging.ImageFormat.Bmp);
+
+                        // Convert to texture
+                        return textureLoader.Load(bmpStream.ToArray());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _log.Error($"Could not load image from icon: '{ex.Message}'.", ex);
+                    return ResourceManager.Instance.DefaultImage;
                 }
             }));
         }
 
-        public static Graphics.Image LoadImageFromIcon(this ResourceManager resourceManager, string cacheKey, System.Drawing.Icon icon)
+        public static Image LoadImageFromIcon(this ResourceManager resourceManager, string cacheKey, System.Drawing.Icon icon)
         {
             return resourceManager.LoadImage(cacheKey, textureLoader => new LazyImage(resourceManager.DefaultImage, () =>
             {
                 var _log = Log.Get<ResourceManager>();
 
-                _log.Info($"Loading image from icon...");
-
-                using (var bmpStream = new MemoryStream())
+                try
                 {
-                    // Convert to bitmap
-                    icon.ToBitmap().Save(bmpStream, System.Drawing.Imaging.ImageFormat.Bmp);
+                    _log.Info($"Loading image from icon...");
 
-                    // Convert to texture
-                    return textureLoader.Load(bmpStream.ToArray());
+                    using (var bmpStream = new MemoryStream())
+                    {
+                        // Convert to bitmap
+                        icon.ToBitmap().Save(bmpStream, System.Drawing.Imaging.ImageFormat.Bmp);
+
+                        // Convert to texture
+                        return textureLoader.Load(bmpStream.ToArray());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _log.Error($"Could not load image from icon: '{ex.Message}'.", ex);
+                    return ResourceManager.Instance.DefaultImage;
                 }
             }));
         }
 
-        public static Graphics.Image LoadImageFromBitmap(this ResourceManager resourceManager, string cacheKey, System.Drawing.Image image)
+        public static Image LoadImageFromBitmap(this ResourceManager resourceManager, string cacheKey, System.Drawing.Image image)
         {
             return resourceManager.LoadImage(cacheKey, textureLoader => new LazyImage(resourceManager.DefaultImage, () =>
             {
                 var _log = Log.Get<ResourceManager>();
-
-                _log.Info($"Loading image from System.Drawing.Image...");
-
-                using (var bmpStream = new MemoryStream())
+                try
                 {
-                    image.Save(bmpStream, System.Drawing.Imaging.ImageFormat.Bmp);
+                    _log.Info($"Loading image from System.Drawing.Image...");
 
-                    // Convert to texture
-                    return textureLoader.Load(bmpStream.ToArray());
+                    using (var bmpStream = new MemoryStream())
+                    {
+                        image.Save(bmpStream, System.Drawing.Imaging.ImageFormat.Bmp);
+
+                        // Convert to texture
+                        return textureLoader.Load(bmpStream.ToArray());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _log.Error($"Could not load image from icon: '{ex.Message}'.", ex);
+                    return ResourceManager.Instance.DefaultImage;
                 }
             }));
         }
